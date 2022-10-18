2fe7f4d6e3eba1c89a3e6c9a0b21f7bcccf5458d
piotrek123411 <83031705+piotrek123411@users.noreply.github.com>
2022-10-18 13:49:55 +0300

    ...р

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -16,11 +16,11 @@ public class CarController {
     @GetMapping("/cars")
     public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
         List<Car> list = new ArrayList<>();
-        list.add(new Car("RangeRover", "White", 777));
-        list.add(new Car("Mercedes", "Black", 171));
-        list.add(new Car("BMW", "Grey", 234));
-        list.add(new Car("Porsche", "Red", 456));
-        list.add(new Car("Jeep", "Cyan", 200));
+        list.add(new Car("RangeRover", "White", 200));
+        list.add(new Car("Mercedes", "Black", 124));
+        list.add(new Car("BMW", "Grey", 545));
+        list.add(new Car("Porsche", "Red", 911));
+        list.add(new Car("Jeep", "Cyan", 100));
         list = CarService.carsCount(list, allCars);
         model.addAttribute("list", list);
         return "car";
